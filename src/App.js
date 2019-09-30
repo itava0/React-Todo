6576eae530686bf9115ced91e4c4d759dd11ce5f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-30 17:02:13 -0400

    Passed down the state to the child components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
+
+const Task = [
+  {
+    task: 'Forum App',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Movie Reviews App',
+    id: 1528817084358,
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      task: Task
+    }
+  }
   render() {
+    console.log(this.state)
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList task={this.state.task}/>
       </div>
     );
   }
